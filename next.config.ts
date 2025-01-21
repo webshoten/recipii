6c71c11090b5972682495004f5c7a817eb30301d
webshoten <weboshimashoten@gmail.com>
2025-01-21 23:42:51 +0900

    ggggg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import { DotenvParseInput, expand } from 'dotenv-expand';
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
+
+//参考 https://laros.io/how-to-get-the-current-url-with-nextjs-on-vercel
+expand({ parsed: { ...(process.env as DotenvParseInput) } });
+
 
 export default nextConfig;
