ece260e6ad58cbb879b532ccd12c3e7b5b31338c
webshoten <weboshimashoten@gmail.com>
2025-01-21 23:49:48 +0900

    xxxx

--- src/lib/getUrl.ts
+++ src/lib/getUrl.ts
@@ -1,6 +1,3 @@
 export const getBaseUrl = () => {
-  if (process.env.NODE_ENV === 'development') {
-    return `http://localhost:3000/`;
-  }
-  return `https://recipii.vercel.app/`;
+  return process.env.NEXT_DEPLOYMENT_URL || '';
 };
