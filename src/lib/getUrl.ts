557a49b218fb9ddad865b74890df6a3c30917318
webshoten <weboshimashoten@gmail.com>
2025-01-21 23:27:02 +0900

    xxx

--- src/lib/getUrl.ts
+++ src/lib/getUrl.ts
@@ -2,5 +2,5 @@ export const getBaseUrl = () => {
   if (process.env.NODE_ENV === 'development') {
     return `http://localhost:3000/`;
   }
-  return `https://${process.env.VERCEL_URL}/`;
+  return `https://recipii-git-main-webshotens-projects.vercel.app/`;
 };
