3f8ab9997805556339894ce3b98ee30b69ddb387
webshoten <weboshimashoten@gmail.com>
2025-02-05 15:54:24 +0900

    xxxxx

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export default defineConfig({
   schema: './src/lib/schema.ts',
-  out: './drizzle',
+  out: './neon/drizzle',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL || '',
